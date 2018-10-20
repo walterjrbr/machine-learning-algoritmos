d7a66e37d9f9dcd8ded0eb092f2073692c0efafc
lab1 <waugustojr@gmail.com>
2018-10-20 14:08:52 -0300

    terceiro commit

--- plot3.py
+++ plot3.py
@@ -13,11 +13,17 @@ def sigmoid_prime(z):
 
 lista1=np.arange(0.0, 30.0, 0.1)
 lista2=[]
+lista3=[]
 print(lista1)
+
 num=sigmoid(np.arange(0.0, 30.0, 0.1))
 print(num)
 lista2=(num)
 
+num_derivada=sigmoid_prime(np.arange(0.0, 30.0, 0.1))
+print(num_derivada)
+lista3=(num_derivada)
 
 plt.plot(lista1,lista2)
+plt.plot(lista1,lista3)
 plt.show()
